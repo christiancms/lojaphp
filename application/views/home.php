aa54f7efc6b43172c239a8ca0f781d4a23e2b9a5
Maicon <infobyte@infobyteinformatica.com>
2016-12-06 21:12:16 -0200

    Banner Esteira em Bootstrap

--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,39 @@
+<style>
+    .carousel-inner > .item > img,
+    .carousel-inner > .item > a > img {
+        width: 100%;
+        margin: auto;
+    }
+</style>
+
+
 <div id="homebody">
-    <div class="alinhado-centro borda-base espaco-vertical">
-        dasdasdasd
-    </div> 
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="assets/img/imagem1.jpg" alt="Chania">
+            </div>
+            <div class="item">
+                <img src="assets/img/imagem2.jpg" alt="Chania">
+            </div>
+            <div class="item">
+                <img src="assets/img/imagem3.jpg" alt="Flower">
+            </div>
+            <div class="item">
+                <img src="assets/img/imagem4.jpg" alt="Flower">
+            </div>
+        </div>
+    </div>
+
 
 
     <div class="row-fluid">
--- application/views/html-header.php
+++ application/views/html-header.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <title>Infobyte Informática</title>
-    <?php
+    <head>
+        <meta charset="utf-8">
+        <title>Infobyte Informática</title>
+        <?php
         echo meta(array('name' => 'viewport', 'content' => 'width=device-width, initial-scale=1.0')) .
         meta(array('name' => 'description', 'content' => 'Infobyte Informática. Vendas e Assistência Técnica, Locação de
         Copiadoras, Xerox, Encadernação, Recarga de Cartuchos')) .
         link_tag('assets/css/bootstrap.min.css') .
         link_tag('assets/css/bootstrap-responsive.min.css') .
         link_tag('assets/css/estilos.css');
-    ?>
-    <script src="<?php echo base_url('assets/js/jquery-2.1.3.min.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
-    <script src="<?php echo base_url('assets/js/jquery.mask.js');?>"></script>
-</head>
-<body>
+        ?>
+        <script src="<?php echo base_url('assets/js/jquery-2.1.3.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets/js/jquery.mask.js'); ?>"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
