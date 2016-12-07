e5e286d9f446f59b5eac3e4281d06328025715c3
Maicon <infobyte@infobyteinformatica.com>
2016-12-07 14:53:44 -0200

    Mudança de Estilo do Rodapé

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,7 @@
+
+<center>
 <div id="footer" class="borda-topo espaco-vertical">
-        &copy; <?php echo date("Y")?> - Infobyte Informática / Fone 53 32751449 / E-mail: infobyte@infobyteinformatica.com
+    &copy; <?php echo date("Y")?> - Av. Narciso Silva, 1169A - Capão do Leão<br> E-mail: infobyte@infobyteinformatica.com <br> Fone: (53) 32751449 
     </div>
 </div>
+</center>
